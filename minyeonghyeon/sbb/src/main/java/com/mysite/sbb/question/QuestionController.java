3b54c3bfb9a116319e585d6c7871c9bd8966360f
min-yeonghyeon <yoon61765@gmail.com>
2024-07-08 17:38:04 +0900

    0.2.1

--- minyeonghyeon/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ minyeonghyeon/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,16 +1,26 @@
 package com.mysite.sbb.question;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
+	private final QuestionRepository questionRepository;
 	
-	@GetMapping("/question/list")
-	@ResponseBody
-	public String list() {
-		return "question list";
-	}
+	
+	@GetMapping("/")
+	public String list(Model model) {
+		List<Question> questionlist = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionlist);
+		return "question_list";
+		}
+	
+	
 }
