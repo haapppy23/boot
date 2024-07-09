49b15263150e5736697bc8d427c515dbc814734e
whoamixavier <whoamixavier@naver.com>
2024-07-09 16:59:38 +0900

    version4

--- kwondaeun/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ kwondaeun/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,21 +2,58 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	private final QuestionRepository questionRepository;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList=this.questionRepository.findAll();
 		model.addAttribute("questionList",questionList);
 		return "question_list";
 	}
+	@Autowired
+	private final QuestionService questionService;
+	
+	@GetMapping(value="/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question=this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
+	}
+	
+	@GetMapping("/create")
+		public String questionCreate() {
+			return "question_form";
+		}
+	
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+        this.questionService.create(subject, content);
+        
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id) {
+		questionService.delete(id);
+		
+		return "redirect:/question/list";
+		
+	}
+	
+	
 }
--- kwondaeun/sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ kwondaeun/sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,7 +1,9 @@
 package com.mysite.sbb.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
 	Question findBySubjectAndContent(String subject, String content);
