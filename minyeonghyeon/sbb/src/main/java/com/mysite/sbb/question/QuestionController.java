3364ba8820d3936aef33f0e94844dcdf28ee074e
min-yeonghyeon <yoon61765@gmail.com>
2024-07-09 16:59:53 +0900

    7/7

--- minyeonghyeon/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ minyeonghyeon/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,22 +5,53 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
+
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
 	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 	
-	
-	@GetMapping("/")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionlist = this.questionRepository.findAll();
 		model.addAttribute("questionList", questionlist);
 		return "question_list";
 		}
 	
+	@GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+	
+	@GetMapping("/create")
+	public String questionCerate() {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String quesstionCreate(@RequestParam(value="subject")String subject,
+									@RequestParam(value="content")String content) {
+		
+		this.questionService.create(subject, content);
+		return "redirect:/question/list";
+	}
+	
+	@GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+       questionService.delete(id);
+        return "redirect:/question/list";
+    }
 	
 }
--- minyeonghyeon/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ minyeonghyeon/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,7 +1,7 @@
 package com.mysite.sbb;
-
-
-
+//
+//
+//
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
 
@@ -25,10 +24,10 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
-	@Autowired
-	private AnswerRepository answerRepository;
+//	@Autowired
+//	private AnswerRepository answerRepository;
 	
-	
+	@Test
 	void testJpa() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요");
@@ -42,47 +41,47 @@ class SbbApplicationTests {
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);
 	}
-		
-		void testJpaa() {
-			List<Question> all = this.questionRepository.findAll();
-			assertEquals(2, all.size());
-			
-			Question q = all.get(0);
-			assertEquals("sbb가 무엇인가요", q.getSubject());
-		}
-		
-		
-		void testjpap() {
-			Optional<Question> oq = this.questionRepository.findById(1);
-			if(oq.isPresent()) {
-				Question q = oq.get();
-				assertEquals("sbb가 무엇인가요",q.getSubject());
-			}
-		}
-		
-		
-		void testjpapj() {
-			Question q = this.questionRepository.findBySubject("sbb가 무엇인가요");
-			assertEquals(1,q.getId());
-		}
-		
-		void testJpapapa() {
-			Question q = this.questionRepository.findBySubjectAndContent(
-	                "sbb가 무엇인가요", "sbb에 대해서 알고 싶습니다");
-	        assertEquals(1, q.getId());
-		}
-		
-		
-		void testjpadel() {
-			assertEquals(2, this.questionRepository.count());
-			Optional<Question> oq = this.questionRepository.findById(1);
-			assertTrue(oq.isPresent());
-			Question q = oq.get();
-			this.questionRepository.delete(q);
-			assertEquals(1, this.questionRepository.count());
-		}
-		
-//		@Test
+//		
+//		void testJpaa() {
+//			List<Question> all = this.questionRepository.findAll();
+//			assertEquals(2, all.size());
+//			
+//			Question q = all.get(0);
+//			assertEquals("sbb가 무엇인가요", q.getSubject());
+//		}
+//		
+//		
+//		void testjpap() {
+//			Optional<Question> oq = this.questionRepository.findById(1);
+//			if(oq.isPresent()) {
+//				Question q = oq.get();
+//				assertEquals("sbb가 무엇인가요",q.getSubject());
+//			}
+//		}
+//		
+//		
+//		void testjpapj() {
+//			Question q = this.questionRepository.findBySubject("sbb가 무엇인가요");
+//			assertEquals(1,q.getId());
+//		}
+//		
+//		void testJpapapa() {
+//			Question q = this.questionRepository.findBySubjectAndContent(
+//	                "sbb가 무엇인가요", "sbb에 대해서 알고 싶습니다");
+//	        assertEquals(1, q.getId());
+//		}
+//		
+//		
+//		void testjpadel() {
+//			assertEquals(2, this.questionRepository.count());
+//			Optional<Question> oq = this.questionRepository.findById(1);
+//			assertTrue(oq.isPresent());
+//			Question q = oq.get();
+//			this.questionRepository.delete(q);
+//			assertEquals(1, this.questionRepository.count());
+//		}
+//		
+//		
 		void testjpaans() {
 			Optional<Answer> oa = this.answerRepository.findById(1);
 	        assertTrue(oa.isPresent());
@@ -91,7 +90,7 @@ class SbbApplicationTests {
 	    }
 		
 		@Transactional
-		 @Test
+		 
 		    void testJpaqeqe() {
 		        Optional<Question> oq = this.questionRepository.findById(2);
 		        assertTrue(oq.isPresent());
