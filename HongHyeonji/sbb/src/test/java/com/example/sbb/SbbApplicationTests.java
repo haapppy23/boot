dd73627dba493cccc419999ba31d288559271c58
hyeonji <ghdguswl13@gmail.com>
2024-07-10 15:53:48 +0900

    3_04 답변 개수 표시하기

--- HongHyeonji/sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ HongHyeonji/sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,20 +1,13 @@
 package com.example.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.sbb.answer.Answer;
 import com.example.sbb.answer.AnswerRepository;
-import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionRepository;
+import com.example.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -25,7 +18,9 @@ class SbbApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 	
-	@Transactional
+	@Autowired
+	private QuestionService questionService;
+	
 	@Test
 	void testJpa() {
 //		Question q1 = new Question();
@@ -82,14 +77,20 @@ class SbbApplicationTests {
 //		Answer a = oa.get();
 //		assertEquals(2, a.getQuestion().getId());
 		
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		
+//		List<Answer> answerList = q.getAnswerList();
+//		
+//		assertEquals(1, answerList.size());
+//		assertEquals("네 자동으로 생성됩니다", answerList.get(0).getContent());
 		
-		List<Answer> answerList = q.getAnswerList();
-		
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다", answerList.get(0).getContent());
+		for(int i=1; i<=300; i++) {
+			String subject =String.format("테스트 데이터 입니다[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 		
 		
 	}
